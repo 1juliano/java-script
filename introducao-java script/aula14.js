d9f45da2340bc0fc0ef4cddb7856aa824b32eee3
1juliano <ftjuliano7@gmail.com>
2026-04-16 18:58:28 -0300

    Add files via upload

---
+++ introducao-java script/aula14.js
@@ -0,0 +1,9 @@
+let botaoMudaTexto = document.querySelector("#btnMudaTexto")
+let titulo = document.querySelector("#titulo")
+
+function mudaTexto() {
+titulo.innerHTML = "bigas"
+}
+
+
+botaoMudaTexto.addEventListener("click" ,mudaTexto)
